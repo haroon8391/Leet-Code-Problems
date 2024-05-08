bcab7213f80d3c6be557153e5ee102cd66333610
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2024-05-08 23:56:59 +0500

    Time: 1 ms (99.30%), Space: 44.9 MB (69.98%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -2,15 +2,14 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> stringList = new ArrayList<>();
         for(int i=1; i<=n; i++){
-            if(i%3 == 0 && i%5 == 0){
+            if(i%3 == 0 && i%5 == 0)
                 stringList.add("FizzBuzz");
-            } else if(i%3 == 0){
+             else if(i%3 == 0)
                 stringList.add("Fizz");
-            } else if(i%5 == 0){
+             else if(i%5 == 0)
                 stringList.add("Buzz");
-            } else{
+             else
                 stringList.add(Integer.toString(i));
-            }
         }
         return stringList;
     }
