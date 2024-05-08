2f8618538322a9e000c13470e5686753f6bdf648
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2024-05-08 23:56:01 +0500

    Time: 1 ms (99.30%), Space: 45.1 MB (24.63%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -4,14 +4,11 @@ class Solution {
         for(int i=1; i<=n; i++){
             if(i%3 == 0 && i%5 == 0){
                 stringList.add("FizzBuzz");
-            }
-            else if(i%3 == 0){
+            } else if(i%3 == 0){
                 stringList.add("Fizz");
-            }
-            else if(i%5 == 0){
+            } else if(i%5 == 0){
                 stringList.add("Buzz");
-            }
-            else{
+            } else{
                 stringList.add(Integer.toString(i));
             }
         }
