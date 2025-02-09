2ca3615525290d43043ed5f03b5b5b45bd5419ff
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2025-02-09 15:19:01 +0500

    Time: 0 ms (100.00%) | Memory: 42.7 MB (45.49%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -9,29 +9,28 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-        if(head1 == null) return head2;
-        if(head2 == null) return head1;
-        
-        ListNode newNode  = new ListNode(0);
-        ListNode curr = newNode;
-        
-        while(head1 != null && head2 != null){
-            if(head1.val <= head2.val){
-                curr.next = head1;
-                head1 = head1.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        ListNode newList = new ListNode(0);
+        ListNode curr = newList;
+
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                curr.next = list1;
+                list1 = list1.next;
             }
             else{
-                curr.next = head2;
-                head2 = head2.next;
+                curr.next = list2;
+                list2 = list2.next;
             }
-            
             curr = curr.next;
         }
-        
-        if(head1 == null) curr.next = head2;
-        if(head2 == null) curr.next = head1;
-        
-        return newNode.next;
+
+        if(list1 != null) curr.next = list1;
+        if(list2 != null) curr.next = list2;
+
+        return newList.next;
     }
 }
