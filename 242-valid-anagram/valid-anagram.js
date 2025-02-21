91422de1152bb7e1f879e139020f054dd799a1f5
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2025-02-21 15:02:18 +0500

    Time: 29 ms (25.84%) | Memory: 58.3 MB (5.14%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+
+    let str1 = s.split("").sort().join("")  
+    let str2 = t.split("").sort().join("") 
+
+    if(str1 === str2) return true;
+
+    return false;
+};
