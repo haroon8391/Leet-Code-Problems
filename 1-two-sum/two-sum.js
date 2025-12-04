8444d256f87a14e2ee4a55a18b2a77647bb10106
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2025-12-04 22:31:01 +0500

    Time: 3 ms (60.36%) | Memory: 56.2 MB (54.55%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -1,23 +1,18 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
+function twoSum(nums, target){
     let ans = []
     let obj = {}
 
-    for(let i=0; i< nums.length; i++){
+    for(let i=0; i<nums.length; i++){
         let rem = target - nums[i]
 
         if(rem in obj){
             ans[0] = obj[rem]
             ans[1] = i;
-            return ans
+            return ans;
         }
 
         obj[nums[i]] = i;
     }
 
     return ans;
-};
+}
