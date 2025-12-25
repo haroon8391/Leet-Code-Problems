32e1b2b848368f815be4c8ee0ebff1db9b27b098
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2025-12-25 22:59:48 +0500

    Time: 0 ms (100.00%) | Memory: 59.8 MB (82.68%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -2,15 +2,17 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    let i = 0;
-    let j = s.length - 1;
-
-    while(i < j){
-        let temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
+function reverseString(arr) {
+    let i =0;
+    let j=arr.length - 1;
+    
+    while(i<j){
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp;
         i++;
         j--;
     }
+    
+    return arr;
 };
