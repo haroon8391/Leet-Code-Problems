8317d743bea3b2ea9526fe652e41314ddf7bf5f3
Muhammad Haroon Ali <111272010+haroon8391@users.noreply.github.com>
2026-02-18 02:27:41 +0500

    Time: 7 ms (11.25%) | Memory: 67 MB (6.82%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -1,19 +1,11 @@
 function maxProfit(prices) {
     let min = prices[0];
-    let max = prices[0];
-    let total = 0;
+    let max = 0
+    ;
     for(let z of prices){
-        if(z < min){
-            min = z;
-            max = z;
-            continue;
-        }
-
-        if(z > max){
-            max = z;
-            total = Math.max(total, max - min)
-        }
+        min = Math.min(min, z);
+        max = Math.max(max, z - min)
     }
 
-    return total;
+    return max;
 };
